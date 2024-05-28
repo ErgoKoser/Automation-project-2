a1c5651338437ce10c85b1fbefca9f0a8b27bf42
ErgoKoser <Ergo121@gmail.com>
2024-05-28 13:09:57 +0300

    Project 2 week 2 Assign 4

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -1,12 +1,22 @@
 //Variables
-const ConfirmWindow = '[data-testid="modal:confirm"]';
-const TrashIcon = '[data-testid="icon:trash"]';
-const IssueDetailView = '[data-testid="modal:issue-details"]';
-const IssueTitle = "This is an issue of type: Task.";
+const confirmWindow = '[data-testid="modal:confirm"]';
+const trashIcon = '[data-testid="icon:trash"]';
+const issueDetailView = '[data-testid="modal:issue-details"]';
+const issueTitle = "This is an issue of type: Task.";
 
-// Function
-function CloseIssueDetailView() {
+// Functions
+function closeIssueDetailView() {
   cy.get(".sc-bdVaJa.fuyACr").click();
+}
+function confirmDeletion() {
+  cy.get('[data-testid="modal:confirm"]').within(() => {
+    cy.contains("Delete issue").click();
+  });
+}
+function cancelDeletion() {
+  cy.get('[data-testid="modal:confirm"]').within(() => {
+    cy.contains("Cancel").click();
+  });
 }
 
 // Assignment 3.
@@ -17,28 +27,28 @@ describe("Issue Deleting", () => {
       .should("eq", `${Cypress.env("baseUrl")}project`)
       .then((url) => {
         cy.visit(url + "/board");
-        cy.contains("This is an issue of type: Task.").click();
+        cy.contains(issueTitle).click();
       });
-    cy.get(IssueDetailView).should("be.visible");
+    cy.get(issueDetailView).should("be.visible");
   });
 
   // Test case 1; Deleting issue
   it("Should delete the selected issue", () => {
-    cy.get(TrashIcon).click();
-    cy.get(ConfirmWindow).should("exist");
-    cy.get("button").contains("Delete issue").click();
-    cy.get(ConfirmWindow).should("not.exist");
+    cy.get(trashIcon).click();
+    cy.get(confirmWindow).should("exist");
+    confirmDeletion();
+    cy.get(confirmWindow).should("not.exist");
     cy.reload();
-    cy.contains(IssueTitle).should("not.exist");
+    cy.contains(issueTitle).should("not.exist");
   });
 
   // Test case 2; Cancel deletion
   it("Should Cancel the delete process", () => {
-    cy.get(TrashIcon).click();
-    cy.get(ConfirmWindow).should("exist");
-    cy.get("button").contains("Cancel").click();
-    cy.get(ConfirmWindow).should("not.exist");
-    CloseIssueDetailView();
-    cy.contains(IssueTitle).should("be.visible");
+    cy.get(trashIcon).click();
+    cy.get(confirmWindow).should("exist");
+    cancelDeletion();
+    cy.get(confirmWindow).should("not.exist");
+    closeIssueDetailView();
+    cy.contains(issueTitle).should("be.visible");
   });
 });
